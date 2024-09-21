6e0583df64ea695ef124b5e1d99f48cf80bdf066
mrsholah <kecantikanwajah25@gmail.com>
2024-09-21 08:25:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fadwa Online Shop</title>
+    <link rel="stylesheet" href="style.css">
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v20.0" nonce="LRhk2fdg"></script>
+</head>
+
+
+<body>
+  <h1>BERIKAN KOMENTAR ANDA</h1>
+  <p> lupakan saja kenangan tentang aku dan kamu oke!</p>
+  <div class="fb-comments" data-href="http://0.0.0.0:8080/latihan%201//index.php" data-width="" data-numposts="100"></div>
+  </body>
+  </html>
